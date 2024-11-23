5132e2d6b28615f6ab2ae42a1b2b733e07e55923
Keith Rincon <keithrt3008@gmail.com>
2024-11-23 13:16:27 -0800

    img src update

--- App.jsx
+++ App.jsx
@@ -1,28 +1,32 @@
 import Contact from './components/Contact';
+import Whiskerson from './src/assets/download.jpg';
+import Fluffykins from './src/assets/download-1.jpg';
+import Felix from './src/assets/download-2.jpg';
+import Pumpkin from './src/assets/download-3.jpg';
 
 export default function App() {
   return (
     <div className='contacts'>
       <Contact
-        img='../src/assets/download.jpg'
+        img={Whiskerson}
         name='Mr. Whiskerson'
         phone='(212) 555-1234'
         email='mr.whiskaz@catnap.meow'
       />
       <Contact
-        img='../src/assets/download-1.jpg'
+        img={Fluffykins}
         name='Fluffykins'
         phone='(212) 555-2345'
         email='fluff@me.com'
       />
       <Contact
-        img='../src/assets/download-2.jpg'
+        img={Felix}
         name='Felix'
         phone='(212) 555-4567'
         email='thecat@hotmail.com'
       />
       <Contact
-        img='../src/assets/download-3.jpg'
+        img={Pumpkin}
         name='Pumpkin'
         phone='(0800) CAT KING'
         email='pumpkin@scrimba.com'
--- Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer>
-      <small>2024 Rincon development. All rights reserved</small>
-    </footer>
-  );
-}
